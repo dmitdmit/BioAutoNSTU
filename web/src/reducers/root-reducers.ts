5a6d3c011203d0c54e974b6daa50ca638a2e22b7
Dmitriev Dmitry <61288438+dmitdmit@users.noreply.github.com>
2021-12-22 00:01:33 +0300

    Delete web directory

--- web/src/reducers/root-reducers.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import devicesReducer from './devices-reducer';
-import usersReducer from './users-reducer';
-
-export default combineReducers({
-    devices: devicesReducer,
-    users: usersReducer,
-});
