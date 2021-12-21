35cf8e4514dcd903144bcb5d99756c02992cd2b9
Dmitriev Dmitry <61288438+dmitdmit@users.noreply.github.com>
2021-12-21 23:41:14 +0300

    Add files via upload

--- broker/webpack.config.js
+++ broker/webpack.config.js
@@ -1,21 +1,58 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
-    target: 'node',
-    entry: './src/index.ts',
+    mode: 'development',
+    entry: './src/index.tsx',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000,
+    },
     module: {
-        rules: [{
-            test: /\.tsx?$/,
-            use: 'babel-loader',
-            exclude: /node_modules/,
-        }],
+        rules: [
+            {
+                test: /\.(tsx|ts)?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', {
+                                targets: '> 2.5%',
+                            }],
+                            ['@babel/preset-typescript'],
+                            ['@babel/preset-react'],
+                        ],
+                    },
+                },
+            }, {
+                test: /\.(css|scss)$/,
+                use: ['style-loader', {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 2,
+                    },
+                }, 'postcss-loader', 'sass-loader'],
+            },
+        ],
     },
     resolve: {
-        extensions: [ '.tsx', '.ts', '.js' ],
+        extensions: ['.tsx', '.ts', '.js'],
+        plugins: [
+            new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })
+        ],
     },
     output: {
-        filename: 'communication-server.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-}
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: false,
+        }),
+    ],
+};
